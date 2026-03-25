b1b2e619c4fc6ad995de6b128ee562b6776dc202
nadellasaideep1 <64152721+nadellasaideep1@users.noreply.github.com>
2026-03-25 07:30:14 +0000

    Disable WebSocket server for Vercel deployment
    WebSockets require persistent connections which don't work in serverless environments

--- artifacts/api-server/src/vercel.ts
+++ artifacts/api-server/src/vercel.ts
@@ -1,3 +1,6 @@
 import app from "./app.js";
 
+// Note: WebSocket server is disabled for Vercel serverless functions
+// WebSockets require persistent connections which don't work in serverless
+
 export default app;
